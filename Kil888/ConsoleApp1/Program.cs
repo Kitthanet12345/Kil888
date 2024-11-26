798094ea2e161facd5c461b5306a50332fb6083a
Kil <Kitthanet.t@kkumail.com>
2024-11-26 11:31:35 +0700

    kil

---
+++ Kil888/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Kitthanert Thanaphiriyaphon";
+string studen_id = "673450932-2"
+// string is Datatype
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Kitthanet Thanaphiriyaphon");
+Console.WriteLine("My student id : 673450392-2");
